a2e9daceb5f6f87b3c49d210dd30e3869a7fee73
Ibram Medhat <ibram.medhat2@gmail.com>
2018-02-18 19:24:06 +0200

    adding tuple class unchanges

---
+++ src/teamName/Tuple.java
@@ -0,0 +1,15 @@
+package teamName;
+
+import java.util.Hashtable;
+
+public class Tuple{
+
+	Hashtable<String, Object> colNameValue;
+	
+	public Tuple(Hashtable<String, Object> htblColNameValue) {
+		
+		this.colNameValue = htblColNameValue;
+		
+	}
+	
+}
